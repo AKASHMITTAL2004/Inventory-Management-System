f1e181bb82e98d2b8827271e4f323b3a04ae52be
AKASH <160458728+AKASHMITTAL2004@users.noreply.github.com>
2026-08-10 19:37:47 +0530

    Update axios.js

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const fallbackURL = "https://advanced-inventory-management-system-v1.onrender.com";
+const fallbackURL = "https://inventory-management-system-s89n.onrender.com";
 
 const axiosInstance = axios.create({
     baseURL: `${process.env.REACT_APP_BACKEND_URL|| fallbackURL}/api`,
