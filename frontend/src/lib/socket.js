65d9ab5baf641c0a9b12cda0771eb2d177eb61b3
AKASH <160458728+AKASHMITTAL2004@users.noreply.github.com>
2026-08-11 10:35:27 +0530

    Update socket connection to use default URL
    Set default socket URL to localhost if backend URL is not defined.

--- frontend/src/lib/socket.js
+++ frontend/src/lib/socket.js
@@ -1,6 +1,8 @@
 import { io } from "socket.io-client";
 
-const socket = io(process.env.REACT_APP_BACKEND_URL); {
+const SOCKET_URL = process.env.REACT_APP_BACKEND_URL || "http://localhost:3003";
+
+const socket = io(SOCKET_URL, {
   withCredentials: true,
   transports: ["websocket", "polling"],
 });
